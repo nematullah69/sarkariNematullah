e4d3cf98a73b2d9c973a65ec6243d02d88afc95d
Md Nematullah <mdchandraj5685@gmail.com>
2025-09-30 23:34:03 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
-import HomePages from "@/Components/HomePages";
+
 
 
 
