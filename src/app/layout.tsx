e7301f494fdb0a395ccf0975ddd7a985bd9c2cc5
mdnematull <nematull69@gmail.com>
2026-01-18 02:30:02 +0530

    Fixed JSX syntax and motion.div errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,6 +91,8 @@ export default function RootLayout({
         ></script>
       
 
+      
+
      <head>
        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8438144860540330"
      crossOrigin="anonymous"></script>
@@ -106,7 +108,7 @@ export default function RootLayout({
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-MRWG0TTSTS"
           strategy="afterInteractive"
-        />
+         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
